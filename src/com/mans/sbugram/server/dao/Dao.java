9063a4d6da89c2669cac384a461f100447c72330
Mans <cando428@gmail.com>
2021-05-26 22:51:11 +0430

    Add UserDao
    Also added test codes.

---
+++ src/com/mans/sbugram/server/dao/Dao.java
@@ -0,0 +1,38 @@
+package com.mans.sbugram.server.dao;
+
+import com.mans.sbugram.server.exceptions.PersistentOperationException;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public interface Dao<T, I> {
+
+    Optional<T> get(I id) throws PersistentOperationException;
+
+    void save(T data) throws PersistentOperationException;
+
+    default Reader getFileReader(String path) throws FileNotFoundException {
+        return new FileReader(path);
+    }
+
+    default Writer getFileWriter(String path) throws IOException {
+        return new FileWriter(path);
+    }
+
+    default List<String> getDirectoryFiles(String path) {
+        File[] result =  new File(path).listFiles(File::isFile);
+
+        if (result == null) {
+            return new ArrayList<>(0);
+        }
+
+        return Arrays.stream(result)
+                .map(File::getAbsolutePath)
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/com/mans/sbugram/server/exceptions/PersistentOperationException.java
@@ -0,0 +1,8 @@
+package com.mans.sbugram.server.exceptions;
+
+public class PersistentOperationException extends Exception {
+
+    public PersistentOperationException(Throwable throwable) {
+        super(throwable);
+    }
+}
