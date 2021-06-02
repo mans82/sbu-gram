e0ad36a03d9e61e66eb3322bf148e67bb1cb08e7
Mans <cando428@gmail.com>
2021-06-02 12:00:31 +0430

    Add client module with a very basic UI
    At this point, the code contains no logic.
    
    Also added run configurations.

---
+++ client/src/com/mans/sbugram/client/Main.java
@@ -0,0 +1,25 @@
+package com.mans.sbugram.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.nio.file.Paths;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Paths.get("views", "LoginSignUp.fxml").toString()));
+
+        Pane rootPane = fxmlLoader.load();
+
+        stage.setScene(new Scene(rootPane));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
